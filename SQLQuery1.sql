8d9d15744fa076067f5b4e88ea042a29df31cfab
Uchechukwu Ogwurumba <39838239+tonero4urch@users.noreply.github.com>
2025-09-09 00:09:31 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,3 +11,10 @@ LEFT JOIN dbo.market_segment$
 ON hotels.market_segment = market_segment$.market_segment
 LEFT JOIN dbo.meal_cost$
 ON meal_cost$.meal = hotels.meal
+
+
+#POWERBI
+Revenue = (stays_in_weekend_nights + stays_in_week_nights)*(adr)*(1-Discount)
+Total Nights = stays_in_weekend_nights + stays_in_week_nights
+Parking Percentage= required_car_parking_spaces/Total Nights
+
